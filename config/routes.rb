88a990e7ae7428805ea5749711eeae1cc8df7fa9
Erik Nguyen <khanh.nguyen@viki.com>
2018-04-26 22:28:20 +0800

    update page links with Rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help', as: 'heoo'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
+  # Auto create 2 named routes:
+  #     help_path -> '/help'
+  #     help_url  -> 'root_url/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
