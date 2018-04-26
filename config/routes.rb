542d962725b21694e5b0c007146b3fa60e55cf5a
Erik Nguyen <khanh.nguyen@viki.com>
2018-04-26 22:48:02 +0800

    add Users controller and signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   get '/help', to: 'static_pages#help', as: 'heoo'
   get '/about', to: 'static_pages#about'
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   # Auto create 2 named routes:
   #     help_path -> '/help'
   #     help_url  -> 'root_url/help'
+
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
