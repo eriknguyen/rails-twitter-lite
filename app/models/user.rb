38fd68f29b850137912a4adc26e999e3ee06c557
Erik Nguyen <khanh.nguyen@viki.com>
2018-04-27 14:36:47 +0800

    create User model with test for validation

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sentitive: false }
+  has_secure_password
+end
