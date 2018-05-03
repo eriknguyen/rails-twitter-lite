ee5e122164d06e35422fda3bdc07532e2f68e35a
Erik Nguyen <khanh.nguyen@viki.com>
2018-05-03 16:45:11 +0800

    add logout function with test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # log out current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil?
+  end    
+
   # returns the current logged-in user (if any)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
