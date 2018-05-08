3aa48819faf33a9ca8ffef98abe241f5f3e43a21
Erik Nguyen <khanh.nguyen@viki.com>
2018-05-08 21:12:14 +0800

    following-users: setup user relationships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,21 @@ class User < ApplicationRecord
   # create association with micropost
   has_many :microposts, dependent: :destroy
 
+  # create association with relationship
+  has_many :active_relationships, class_name: "Relationship",
+           foreign_key: "follower_id",
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
   # basic setup for User model
-  before_save { self.email = email.downcase }
+  before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sentitive: false }
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sentitive: false}
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   # Defines a proto-feed
   # see "following users" for the full implementation
