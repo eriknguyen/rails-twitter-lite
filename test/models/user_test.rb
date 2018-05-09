74d7fa2a17e59f808e26ec58021f676ea9e00535
Erik Nguyen <khanh.nguyen@viki.com>
2018-05-09 14:35:52 +0800

    following-users: update user post feeds; use subselects for getting following_ids on database

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -115,4 +115,23 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
   end
 
+  # Test user post feed
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # posts from unfollowed users
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
