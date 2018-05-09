fc4d51eef380d36f27b68c5824095ed979b44982
Erik Nguyen <khanh.nguyen@viki.com>
2018-05-09 14:42:42 +0800

    add test feed on homepage

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -54,4 +54,11 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "feed on Homepage" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      # assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
+
 end
