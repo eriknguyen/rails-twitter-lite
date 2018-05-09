8b9e52ccbd92f05f817c4b71dcf80a6527fc0478
Erik Nguyen <khanh.nguyen@viki.com>
2018-05-09 11:57:30 +0800

    following-users: add following/followers view

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
     end
   end
   resources :microposts,      only: [:create, :destroy]
-  resources :relationships,   only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
